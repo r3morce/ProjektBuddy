45e78f4278cc95a5040318910aa943cc07050b5f
Mathias <roteaugen@gmx.net>
2018-03-17 12:19:08 +0100

    Initial Commit

---
+++ WestApp/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  WestApp
+//
+//  Created by mathias@privat on 17.03.18.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
